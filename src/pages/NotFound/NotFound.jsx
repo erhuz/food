7c91f9720e958c763c1e9cb219f529bd2a83360a
Benjamin Fransson <benjamin@cafcon.se>
2020-02-22 20:34:01 +0100

    Scaffolded pages

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = (props) => {
+
+  console.log(props);
+
+
+  return (
+    <div> 404 Not Found </div>
+  );
+}
+
+export default NotFound;
---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Welcome = (props) => {
+  return (
+    <div> Welcome user! </div>
+  );
+}
+
+export default Welcome;
