c2a797fbd991063bdbc3156e4a992760f71f2bc9
Benjamin Fransson <benjamin@cafcon.se>
2020-02-23 21:44:42 +0100

    Added container animation for menu

--- src/partials/Navbar/Navbar.jsx
+++ src/partials/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Nav } from "grommet";
+import { Nav, Box } from "grommet";
 import {
   Home as HomeIcon,
-  ChatOption as ChatOptionIcon,
-  Notification as NotificationIcon,
+  UserSettings as UserSettingsIcon,
+  Cafeteria as CafeteriaIcon,
 } from "grommet-icons";
 import { Link } from 'react-router-dom';
 
@@ -20,22 +20,28 @@ const marginStyles = {
 
 const Navbar = ({items}) => {
 
+  const containerAnimation = {
+    "type": "fadeIn",
+    "delay": 0,
+    "duration": 500,
+    "size": "xsmall"
+  }
 
   return (
-    <>
+    <Box animation={containerAnimation}>
       <Nav style={navStyles} direction="row" justify="around" background="brand" pad="medium">
-        <Link to='/test'>
+        <Link to='/'>
           <HomeIcon />
         </Link>
-        <Link to='/test'>
-          <NotificationIcon />
+        <Link to='/food'>
+          <CafeteriaIcon />
         </Link>
-        <Link to='/test'>
-          <ChatOptionIcon />
+        <Link to='/settings'>
+          <UserSettingsIcon />
         </Link>
       </Nav>
       <div style={marginStyles}></div>
-    </>
+    </Box>
   );
 }
 
