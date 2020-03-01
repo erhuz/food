bcc3c86879bae3214c7b31fb2240882a6f780efb
Benjamin Fransson <benjamin@cafcon.se>
2020-03-01 11:52:12 +0100

    Removed unnecessary code & modified logic.

--- src/pages/Food/Food.jsx
+++ src/pages/Food/Food.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Box, Stack, Button, Heading, Header, Paragraph } from 'grommet';
 import { Add as AddIcon } from 'grommet-icons';
+import { useToasts } from 'react-toast-notifications';
 
 const Food = (props) => {
-
-  console.log(props);
+  const { addToast } = useToasts();
 
   const wrapperStyles = {
     background: 'repeating-linear-gradient(135deg,rgba(253,111,255,0.4) 0%,rgba(129,252,237,0.4) 55%,rgba(111,255,176,0.4) 100%)',
@@ -22,15 +22,9 @@ const Food = (props) => {
     borderRadius: '50%'
   }
 
-  const cardStyles = {
-    // border: '1px solid #999999',
-    // background: '#F2F2F2',
-    borderRadius: 8,
-  }
-
   const cardBackground = {
     "color": "neutral-1",
-    "dark": 0.2,
+    "dark": true,
     "opacity": 0.4,
     "position": "bottom",
     "image": "url(https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80)"
@@ -38,22 +32,18 @@ const Food = (props) => {
 
   const innerCardStyles = {
     backgroundColor: 'rgba(34,34,34,0.75)',
-    borderRadius: 8,
   }
 
-  let meals = [];
+  let fakeMeals = [];
 
   for (let index = 0; index < 15; index++) {
-    meals.push(
+    fakeMeals.push(
       <Stack anchor="top-right">
-        <Box margin="medium" elevation="medium" style={cardStyles} background={cardBackground}>
-          <Box pad="large" style={innerCardStyles}>
+        <Box margin="medium" round={true} elevation="medium" background={cardBackground}>
+          <Box pad="large" round={true} style={innerCardStyles}>
             <Heading margin="xsmall" level="4">Tacogratäng</Heading>
             <Paragraph>
-              Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua.
+              Lättlagad och god tacogratäng som serveras med picklad rödlök på toppen.
             </Paragraph>
           </Box>
         </Box>
@@ -64,6 +54,7 @@ const Food = (props) => {
           pad="xsmall"
           size="large"
           gap="medium"
+          onClick={() => { addToast('Added a portion to the list!', {appearance: 'success', autoDismiss: 2000})}}
         />
       </Stack>
     );
@@ -71,12 +62,12 @@ const Food = (props) => {
   }
 
   return (
-    <Box fill={true} animation={wrapperAnimation} style={wrapperStyles}>
+    <Box fill={true} animation={wrapperAnimation} round={true} style={wrapperStyles}>
       <Header background="brand" justify="center">
         <Heading level="2">What do you want to eat?</Heading>
       </Header>
       <Box justify="start" pad="small">
-        {meals}
+        {fakeMeals}
       </Box>
     </Box>
   );
