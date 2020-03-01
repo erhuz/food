f2709744f3e1e710497e9e3bfff417d177a70178
Benjamin Fransson <benjamin@cafcon.se>
2020-03-01 11:41:51 +0100

    production build

---
+++ build/precache-manifest.55e26175e28f72e1ffa8d598da87f16c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf0bdc5892c4b40834024f5dbb1f497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d6e0af05fffa7f17239",
+    "url": "/static/css/main.bcda1bd5.chunk.css"
+  },
+  {
+    "revision": "bed7b25a467fd12574be",
+    "url": "/static/js/2.cc954301.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.cc954301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6e0af05fffa7f17239",
+    "url": "/static/js/main.f1322b8b.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
